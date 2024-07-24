9f335a9c9feab3c12d863638294068e1456e2616
sadiq-1698 <mohammedsadiq1698@gmail.com>
2024-07-24 08:25:23 +0530

    CRUD operations for Review model

--- src/main/java/com/sadiq1698/firstjobapp/review/impl/ReviewServiceImpl.java
+++ src/main/java/com/sadiq1698/firstjobapp/review/impl/ReviewServiceImpl.java
@@ -9,7 +9,6 @@ import com.sadiq1698.firstjobapp.review.ReviewService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ReviewServiceImpl implements ReviewService {
